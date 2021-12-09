9b4e54372d886c978855ca0d1c114f303ae87535
sakshi <tiwari2000sakshi@gmail.com>
2021-12-09 14:15:40 +0530

    implemeted plus and dlete feature

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
+import Task from "./components/Task.js";
 
 export default function App() {
+  const [text, setText] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+  const makeid = () => {
+    var Text = " ";
+    var possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 5; i++)
+      Text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    setTaskItems([...taskItems, Text]);
+  };
+  const deleteTask = (index) => {
+    let copyTask = [...taskItems];
+    copyTask.splice(index, 1);
+    setTaskItems(copyTask);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Shopping List</Text>
+        <View style={styles.writeTaskWrapper}>
+          <TextInput style={styles.input} placeholder={"write a task"} />
+          <TouchableOpacity onPress={() => makeid()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.items}>
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => deleteTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +55,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: { paddingTop: 80, paddingHorizontal: 20 },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 80,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    paddingTop: 120,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: 60,
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
   },
 });
